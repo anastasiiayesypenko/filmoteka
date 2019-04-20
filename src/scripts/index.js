8ca48665b1c484d2d6e568e9223f3791bf1d5f9d
Vanchurick <davane4ek@gmail.com>
2019-04-20 11:27:34 +0300

    made library html

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,11 +3,13 @@ import '../styles/style.sass';
 import SearchView from './search-section/search-section-view';
 import SearchModel from './search-section/search-section-model';
 import SearchController from './search-section/search-section-controller';
+import Show from './library-section/library';
 
 const model = new SearchModel();
 const view = new SearchView();
 let controller = new SearchController(model, view);
 
+let show = Show();
 
 
 
---
+++ src/scripts/library-section/library.js
@@ -0,0 +1,33 @@
+'use strict';
+
+
+
+
+export default function Show () {
+    let container = document.querySelector('.container');
+    container.innerHTML = '';
+
+    let linksList = document.createElement('ul');
+    let itemQueue = document.createElement('li');
+    let itemFavorites = document.createElement('li');
+    let itemHaveSeen = document.createElement('li');
+    let linkQueue = document.createElement('a');
+    let linkFavorites = document.createElement('a');
+    let linkHaveSeen = document.createElement('a');
+    let moviesCards = document.createElement('div');
+
+    moviesCards.classList.add('js-movies-cards');
+    moviesCards.classList.add('container');
+
+    linkQueue.textContent = 'Очередь просмотра';
+    linkFavorites.textContent = 'Избранные';
+    linkHaveSeen.textContent = 'Просмотренные';
+
+
+    container.append(linksList);
+    itemQueue.append(linkQueue);
+    itemFavorites.append(linkFavorites);
+    itemHaveSeen.append(linkHaveSeen);
+    linksList.append(itemQueue, itemFavorites, itemHaveSeen);
+    container.append(moviesCards);
+}
