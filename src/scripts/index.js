d497e7962c2f576bec5186d1ba48029b9cc80baf
Anastasia <nastiyayesypenko@ukr.net>
2019-04-21 16:31:24 +0300

    popstate

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,21 +8,11 @@ import FilmModel from './show-movie/film-model';
 import FilmController from './show-movie/film-controller';
 import Library from './library-section/library';
 
+window.addEventListener('popstate', onPopState);
+function onPopState() {
+    
+}
 const model = new SearchModel();
 const view = new SearchView();
 const controller = new SearchController(model, view);
 
-let hstr = history.state;
-let rarararara = window.history;
-let numberOfEntries = window.history.length;
-console.log('aaaaaa', document.location);
-
-console.log(hstr, numberOfEntries, 'window.history', window.history, rarararara);
-
-// const filmModel = new FilmModel();
-// const filmView = new FilmView();
-// let filmController = new FilmController(filmModel, filmView);
-
-// let controller = new SearchController(model, view);
-
-// const library = new Library();
