71ecfe00dd4a854370f7da9fe6876e7089215467
Anastasia <nastiyayesypenko@ukr.net>
2019-04-24 01:40:30 +0300

    history search saved

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,12 +3,6 @@ import '../styles/style.sass';
 import SearchView from './search-section/search-section-view';
 import SearchModel from './search-section/search-section-model';
 import SearchController from './search-section/search-section-controller';
-import FilmView from './show-movie/film-view';
-import FilmModel from './show-movie/film-model';
-import FilmController from './show-movie/film-controller';
-import Library from './library-section/library';
-
-
 
 
 const model = new SearchModel();
